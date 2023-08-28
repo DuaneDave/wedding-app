6bc3ecf42cec35b9c17540130d228bfaeee15212
duanedave <duanedave17@gmail.com>
2023-08-28 20:47:35 +0100

    Add fixed header on scroll

--- component/navigation/Navigation.tsx
+++ component/navigation/Navigation.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { motion, AnimatePresence } from 'framer-motion';
 
@@ -11,9 +11,24 @@ import CancelIcon from '@/public/svg/cancelIcon';
 
 function Navigation() {
   const [open, setOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [open]);
 
   return (
-    <header>
+    <header className={`full-width ${scrolled ? styles.scrolled : ''}`}>
       <div className={`flex center ${styles.notice}`}>
         <motion.span
           animate={{
