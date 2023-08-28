8d95fc79552d8f238da59e54aedcc4972a8a6a8c
duanedave <duanedave17@gmail.com>
2023-08-28 19:21:57 +0100

    Add the event section

---
+++ component/homepage/Event.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import styles from './homepage.module.css';
+
+import Underline from '@/public/svg/underline';
+import Date from './Date';
+import Arrows from '../ui/Arrows';
+
+function Event() {
+  return (
+    <section>
+      <div className={`container full-width ${styles.eventContainer}`}>
+        <div className={` flex flex-col ${styles.event}`}>
+          <div className={`flex flex-col ${styles.details}`}>
+            <div>
+              <div className={styles.headers}>
+                <h2>The Date</h2>
+                <Underline />
+              </div>
+
+              <p className={styles.eventText}>
+                Saturday, <br /> June 23rd 2023
+              </p>
+            </div>
+            <Date />
+          </div>
+
+          <div className={`flex ${styles.details}`}>
+            <Arrows />
+            <div>
+              <div className={styles.headers}>
+                <h2>The Date</h2>
+                <Underline />
+              </div>
+
+              <p className={styles.eventText}>
+                The Dorchester <br /> Event Center
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Event;
