90d1045fad815546cff8074f96f479b3107b7ab8
duanedave <duanedave17@gmail.com>
2023-08-28 19:22:10 +0100

    Render the event component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Loader from '@/component/Loader';
 import HeroSection from '@/component/homepage/HeroSection';
+import Event from '@/component/homepage/Event';
 
 export default function Home() {
   return (
     <>
       <Loader />
       <HeroSection />
+      <Event />
     </>
   );
 }
