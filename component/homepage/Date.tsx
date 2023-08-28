88d1e67c16e37a37cabc06d1ed78c22c5730a1d6
duanedave <duanedave17@gmail.com>
2023-08-28 16:16:56 +0100

    Create a reusable date component

---
+++ component/homepage/Date.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from './homepage.module.css';
+
+function Date({ className }: { className?: string }) {
+  return (
+    <span className={`flex flex-col ${styles.date} ${className}`}>
+      <p>10</p>
+      <p>/</p>
+      <p>23</p>
+    </span>
+  );
+}
+
+export default Date;
