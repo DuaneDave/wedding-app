442cbcd36582b3736a633e1cc92416d430ff118d
Jessica Ikechukwu <jessi.ikechukwu@gmail.com>
2023-09-25 16:16:26 +0100

    Update Date.tsx

--- component/homepage/Date.tsx
+++ component/homepage/Date.tsx
@@ -5,7 +5,7 @@ import styles from './homepage.module.css';
 function Date({ className }: { className?: string }) {
   return (
     <span className={`flex flex-col ${styles.date} ${className}`}>
-      <p>02</p>
+      <p>12</p>
       <p>/</p>
       <p>23</p>
     </span>
